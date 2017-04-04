6e0ea51df753d56863376c18f50b160861a9afff
cwanda <wanda@happycity.com>
2017-04-04 06:22:33 -0700

    test

--- newt.rb
+++ newt.rb
@@ -1,16 +1,45 @@
+
 class Newt < Formula
-  desc "Package, build and installation management system for Mynewt OS embedded applications"
+  desc "Package, build and installation system for Mynewt OS applications"
   homepage "https://github.com/cwanda/incubator-mynewt-newt"
-  url "https://github.com/apache/incubator-mynewt-newt.git"
+  url "https://github.com/apache/incubator-mynewt-newt.git",
+      :tag => "mynewt_1_0_0_tag"
+      :revision => "c42ebdce8c278a00fa8d68ed342c45cd22e44dfb"
+  version "1.0.0"
 
+  head "https://github.com/apache/incubator-mynewt-newt.git"
 
-  bottle :uneeded
+  bottle :unneeded
 
   depends_on "go" => :build
+  depends_on "gdb" => [:build, "with-python"]
+  depends_on "PX4/homebrew-px4/gcc-arm-none-eabi-49" => :build
 
   def install
-    ENV["GOPATH"] = system "pwd"
-    system "go", "install"
+    contents = Dir["{*,.git,.gitignore}"]
+
+    gopath = buildpath/"gopath"
+    (gopath/"src/mynewt.apache.org/newt").install contents
+    ENV["GOPATH"] = gopath
+    ENV.prepend_create_path "PATH", gopath/"bin"
+
+    cd gopath/"src/mynewt.apache.org/newt/newt" do
+      system "go", "install"
+      bin.install gopath/"bin/newt"
+    end
+
+    cd gopath/"src/mynewt.apache.org/newt/newtmgr" do
+      system "go", "install"
+      bin.install gopath/"bin/newtmgr"
+    end
+
+    cd gopath/"src/mynewt.apache.org/newt/newtvm" do
+      system "go", "install"
+      bin.install gopath/"bin/newtvm"
+    end
   end
 
+  test do
+    system bin/"newt", "version"
+  end
 end
